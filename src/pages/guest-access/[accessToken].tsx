7f69a7d30346eb1ed374ca49d09112ada946c013
Valeriia Klestova <v.s.apukhtina@gmail.com>
2023-09-08 13:58:57 +0200

    added functionality to authorize guest and push them to my-plants page; debugged code and added router.query.accessToken to dependecy array for useEffect to trigger reload when query object is ready

--- src/pages/guest-access/[accessToken].tsx
+++ src/pages/guest-access/[accessToken].tsx
@@ -1,5 +1,4 @@
 import axios from "axios";
-import router from "next/router";
 import { useEffect, useState } from "react";
 import {
   AlertDialog,
@@ -12,44 +11,102 @@ import {
   AlertDialogTitle,
   AlertDialogTrigger,
 } from "@/components/ui/alert-dialog";
+import { useRouter } from "next/router";
+import { z } from "zod";
+
+const checkUserInfo = z.object({
+  name: z.string(),
+  email: z.string().email(),
+  photo: z.string().nullable(),
+});
+type UserInfo = z.infer<typeof checkUserInfo>;
 
 const GuestAuth = () => {
-  const accessToken = router.query.accessToken;
+  const router = useRouter();
   const [isGuesToken, setGuestToken] = useState<boolean>(false);
-  const [count, setCount] = useState<number>(0);
+  const [loggedUser, setLoggedUser] = useState<UserInfo | null>(null);
   useEffect(() => {
-    const tokenFromLocalStorage: string | null = localStorage.getItem("token");
-    if (!tokenFromLocalStorage) {
+    const accessToken = router.query.accessToken;
+    if (!accessToken) {
+      return;
+    }
+    console.log(accessToken);
+    console.log(router.query.accessToken);
+    const tokenFromLocalStorage: string | null | undefined =
+      localStorage.getItem("token");
+    if (
+      (tokenFromLocalStorage == null || tokenFromLocalStorage == undefined) &&
+      accessToken != undefined
+    ) {
       try {
         const validateToken = async () => {
           const response = await axios.get(
             `http://localhost:8000/access-tokens/authorize/${accessToken}`
           );
           localStorage.setItem("token", response.data.guest_token);
-          setGuestToken(true);
         };
         validateToken();
+        router.push("/my-plants");
       } catch (error) {
         console.log(error);
       }
-    } else {
-      setGuestToken(false);
+    } else if (
+      tokenFromLocalStorage != null &&
+      tokenFromLocalStorage != undefined
+    ) {
+      try {
+        const getUserFromApi = async (token: string) => {
+          const response = await axios.get("http://localhost:8000/me", {
+            headers: { Authorization: `Bearer ${token}` },
+          });
+          const parsedResponse = checkUserInfo.safeParse(response.data);
+          if (parsedResponse.success === true) {
+            setLoggedUser(parsedResponse.data);
+          } else {
+            console.log(parsedResponse.error.flatten());
+          }
+        };
+        getUserFromApi(tokenFromLocalStorage);
+        setGuestToken(false);
+      } catch (error) {
+        console.log(error);
+      }
     }
-  }, [count]);
+  }, [isGuesToken, router.query.accessToken]);
   const handleLogOutClick = () => {
     localStorage.removeItem("token");
-    setCount(count + 1);
+    setGuestToken(true);
   };
+  if (!loggedUser) {
+    return (
+      <main className="h-screen flex justify-center bg-[#57886C]">
+        <div>
+          <p className="text-white mt-32">
+            We are checking your credentials, it can take some time...
+          </p>
+        </div>
+      </main>
+    );
+  }
   if (isGuesToken === false) {
     return (
       <AlertDialog>
-        <AlertDialogTrigger asChild>{isGuesToken === false}</AlertDialogTrigger>
+        <AlertDialogTrigger asChild>
+          <div className="h-screen bg-[url('/plant.jpg')] bg-center bg-no-repeat bg-cover flex justify-center items-start">
+            <div className="flex flex-col justify-center mt-32 rounded-lg p-6 shadow-xl backdrop-blur-md bg-gray-900/10 text-white">
+              <p className="m-2">Please, click to log in as a guest</p>
+              <button className="border-[1px] border-white border-dashed p-2 mx-2 my-4 rounded-md hover:bg-sky-100/20 active:border-solid active:bg-[#81A684]">
+                Authorize me as a guest
+              </button>
+            </div>
+          </div>
+        </AlertDialogTrigger>
         <AlertDialogContent>
           <AlertDialogHeader>
             <AlertDialogTitle>Are you absolutely sure?</AlertDialogTitle>
             <AlertDialogDescription>
-              You are already logged in. By clicking "Continue" you agree to be
-              logged out and logged in as a guest.
+              You are already logged in as {loggedUser.name}. By clicking
+              "Continue" you agree to be logged out and logged in as a guest.
             </AlertDialogDescription>
           </AlertDialogHeader>
           <AlertDialogFooter>
@@ -63,9 +120,11 @@ const GuestAuth = () => {
     );
   }
   return (
-    <main className="h-screen flex justify-center items-center">
+    <main className="h-screen flex justify-center items-center bg-[#57886C]">
       <div>
-        <p>We are checking your credentials, it can take some time...</p>
+        <p className="text-white">
+          We are checking your credentials, it can take some time...
+        </p>
       </div>
     </main>
   );
